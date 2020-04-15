d14681e424367905b3878d4b9ee3c741a59662c1
Judith Kahrer <judith@curity.io>
2020-04-15 10:55:29 +0200

    Support refresh token flow

--- src/main/java/io/curity/example/oidcspringbootmutualtls/Oauth2LoginController.java
+++ src/main/java/io/curity/example/oidcspringbootmutualtls/Oauth2LoginController.java
@@ -4,7 +4,6 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/main/java/io/curity/example/oidcspringbootmutualtls/SecurityConfig.java
+++ src/main/java/io/curity/example/oidcspringbootmutualtls/SecurityConfig.java
@@ -2,6 +2,7 @@ package io.curity.example.oidcspringbootmutualtls;
 
 import io.netty.handler.ssl.SslContext;
 import io.netty.handler.ssl.SslContextBuilder;
+import org.springframework.boot.web.server.Ssl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -9,8 +10,10 @@ import org.springframework.http.client.reactive.ClientHttpConnector;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.client.endpoint.OAuth2AuthorizationCodeGrantRequest;
+import org.springframework.security.oauth2.client.endpoint.OAuth2RefreshTokenGrantRequest;
 import org.springframework.security.oauth2.client.endpoint.ReactiveOAuth2AccessTokenResponseClient;
 import org.springframework.security.oauth2.client.endpoint.WebClientReactiveAuthorizationCodeTokenResponseClient;
+import org.springframework.security.oauth2.client.endpoint.WebClientReactiveRefreshTokenTokenResponseClient;
 import org.springframework.security.oauth2.client.oidc.authentication.OidcIdTokenValidator;
 import org.springframework.security.oauth2.client.oidc.authentication.ReactiveOidcIdTokenDecoderFactory;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
@@ -37,6 +40,17 @@ import java.time.temporal.ChronoUnit;
 import static org.springframework.security.config.Customizer.withDefaults;
 import static org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder.withJwkSetUri;
 
+/**
+ * This class configures the security setting for the application.
+ * There are several web clients involved in the different parts of the OAuth 2.0 flow.
+ * Every web client must trust the OAuth 2.0 server certificate and be updated with a custom trust store (if so configured).
+ * Every web client making a request to the OAuth 2.0 server that requires authentication must be setup for mutual TLS.
+ *
+ * Note: This configuration will only work for OAuth 2.0 clients that use the authorization code flow and refresh tokens.
+ *
+ * Take into account that this example will significantly change when the following issue gets solved:
+ * - https://github.com/spring-projects/spring-security/issues/4498
+ */
 @Configuration
 @Import(TrustStoreConfig.class)
 public class SecurityConfig {
@@ -61,6 +75,7 @@ public class SecurityConfig {
     /**
      * Creates a jwt/id token decoder factory that uses the given trust for retrieving the JWKS.
      * If no trust was configured use default implementation instead.
+     *
      * @return
      * @throws IOException
      * @throws CertificateException
@@ -84,8 +99,7 @@ public class SecurityConfig {
 
     /**
      * Creates an access token response client that handles the authorization code flow.
-     * This client supports mutual TLS and therefore requires a client certificate and key provided by the key manager factory.
-     * It also must trust the server certificate which is provided through the trust manager factory. If not trust was configured default is used.
+     * This client supports mutual TLS and therefore requires a client certificate and key.
      *
      * @return
      * @throws IOException
@@ -95,17 +109,7 @@ public class SecurityConfig {
      */
     @Bean
     ReactiveOAuth2AccessTokenResponseClient<OAuth2AuthorizationCodeGrantRequest> reactiveOAuth2AccessTokenResponseClientWithMtls() throws IOException, CertificateException, NoSuchAlgorithmException, KeyStoreException, UnrecoverableKeyException {
-        TrustManagerFactory trustManagerFactory = trustStoreConfig.trustManagerFactory();
-
-        SslContextBuilder sslContextBuilder = SslContextBuilder
-                .forClient()
-                .keyManager(trustStoreConfig.keyManagerFactory());
-
-        if (trustManagerFactory != null) {
-            sslContextBuilder.trustManager(trustManagerFactory);
-        }
-
-        SslContext sslContext = sslContextBuilder.build();
+        SslContext sslContext = createMutualTlsContext();
 
         WebClientReactiveAuthorizationCodeTokenResponseClient mtlsClient = new
                 WebClientReactiveAuthorizationCodeTokenResponseClient();
@@ -116,7 +120,61 @@ public class SecurityConfig {
         return mtlsClient;
     }
 
-    WebClient createWebClient(SslContext sslContext) {
+    /**
+     * Create an access token response client that handles the refresh token flow.
+     * This client supports mutual TLS and therefore requires a client certificate and key.
+     *
+     * @return
+     * @throws UnrecoverableKeyException
+     * @throws CertificateException
+     * @throws NoSuchAlgorithmException
+     * @throws KeyStoreException
+     * @throws IOException
+     */
+    @Bean
+    ReactiveOAuth2AccessTokenResponseClient<OAuth2RefreshTokenGrantRequest> reactiveOAuth2AccessTokenResponseClientWithMtlsAndRefreshToken() throws UnrecoverableKeyException, CertificateException, NoSuchAlgorithmException, KeyStoreException, IOException {
+        SslContext sslContext = createMutualTlsContext();
+
+        WebClientReactiveRefreshTokenTokenResponseClient mtlsClient = new
+                WebClientReactiveRefreshTokenTokenResponseClient();
+
+        WebClient mtlsWebClient = createWebClient(sslContext);
+        mtlsClient.setWebClient(mtlsWebClient);
+
+        return mtlsClient;
+    }
+
+    /**
+     * Creates the prerequisites for mutual TLS. This method adds the client certificate and corresponding key as well as a custom trust store to the context.
+     * If no custom trust store was configured JVM default settings are used.
+     *
+     * @return
+     * @throws UnrecoverableKeyException
+     * @throws CertificateException
+     * @throws NoSuchAlgorithmException
+     * @throws KeyStoreException
+     * @throws IOException
+     */
+    private SslContext createMutualTlsContext() throws UnrecoverableKeyException, CertificateException, NoSuchAlgorithmException, KeyStoreException, IOException {
+        TrustManagerFactory trustManagerFactory = trustStoreConfig.trustManagerFactory();
+
+        SslContextBuilder sslContextBuilder = SslContextBuilder
+                .forClient()
+                .keyManager(trustStoreConfig.keyManagerFactory());
+
+        if (trustManagerFactory != null) {
+            sslContextBuilder.trustManager(trustManagerFactory);
+        }
+
+        return sslContextBuilder.build();
+    }
+
+    /**
+     * Create a web client with the given ssl context. That way you can add custom trust or client certificate to the web client.
+     * @param sslContext
+     * @return
+     */
+    private WebClient createWebClient(SslContext sslContext) {
 
         HttpClient nettyClient = HttpClient
                 .create(ConnectionProvider.create("small-test-pool", 3))
@@ -134,7 +192,7 @@ public class SecurityConfig {
 
     /**
      * Based on {@link ReactiveOidcIdTokenDecoderFactory}
-     * Returns a jwtDecoder with an updated webClient
+     * Returns a jwtDecoder with an updated webClient. Works only when jwkSetUri is configured and no MAC based algorithm was used.
      */
     private class UpdatedReactiveJwtDecoderFactory implements ReactiveJwtDecoderFactory<ClientRegistration> {
 
